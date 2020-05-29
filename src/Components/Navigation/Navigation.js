73658bb1a74fcd36b1f89f53dee3af5edd44d42d
JM Coding <jakubmeri@gmail.com>
2020-05-30 01:07:33 +0200

    Update package.json

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -7,8 +7,8 @@ import './Navigation.css'
 export default function Navigation() {
     return (
         <nav>
-            <Link  to="/" className="logo">MSPORTS</Link>
-            <NavLink activeClassName="active" exact to="/" className="nav-item">Home</NavLink>
+            <Link  to="/Home" className="logo">MSPORTS</Link>
+            <NavLink activeClassName="active" exact to="/Home" className="nav-item">Home</NavLink>
             <NavLink activeClassName="active"  to="/BMR" className="nav-item">Bmr Calculator</NavLink>
             <NavLink activeClassName="active"  to="/About" className="nav-item">About</NavLink>
         </nav>
