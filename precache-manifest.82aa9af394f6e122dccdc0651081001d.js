a258a9c0da2869f5506067a0f48d36c306984f5d
JM Coding <jakubmeri@gmail.com>
2020-05-30 01:05:22 +0200

    Updates

---
+++ precache-manifest.82aa9af394f6e122dccdc0651081001d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcf0d7654a0ec2278585995eb8626a2",
+    "url": "/MSPORTS/index.html"
+  },
+  {
+    "revision": "b33e177137df7f05a175",
+    "url": "/MSPORTS/static/css/main.91049a09.chunk.css"
+  },
+  {
+    "revision": "44388decf30f9d1ba957",
+    "url": "/MSPORTS/static/js/2.134c086a.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/MSPORTS/static/js/2.134c086a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33e177137df7f05a175",
+    "url": "/MSPORTS/static/js/main.2a47c619.chunk.js"
+  },
+  {
+    "revision": "848e0cc11a38d4fe31d7",
+    "url": "/MSPORTS/static/js/runtime-main.040ac8db.js"
+  },
+  {
+    "revision": "00d7569b6b9d55b9d9e59696118126d2",
+    "url": "/MSPORTS/static/media/bg.00d7569b.jpeg"
+  }
+]);
