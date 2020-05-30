e39345e8ed9c0ed9f7fd05cc810f7de5b2061611
JM Coding <jakubmeri@gmail.com>
2020-05-30 11:26:43 +0200

    mobile

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Link, NavLink
   } from "react-router-dom";
 import './Navigation.css'
 
 export default function Navigation() {
+
+    const [menu, setMenu] = useState(false);
+    
+    const showMenu = () => {
+        let set = !menu;
+        setMenu(set)
+    }
+
     return (
-        <nav>
+        <div>
+            <button onClick={showMenu} className="btn">{!menu ? <i class="fas fa-bars"></i> : <i class="fas fa-times"></i>}</button>
+            <Link to="/MSPORTS" className="mobile-logo">MSPORTS</Link>
+            <nav className={menu ? "show" : " hide"}>
             <Link  to="/MSPORTS" className="logo">MSPORTS</Link>
-            <NavLink activeClassName="active" exact to="/MSPORTS" className="nav-item">Home</NavLink>
-            <NavLink activeClassName="active"  to="/BMR" className="nav-item">Bmr Calculator</NavLink>
-            <NavLink activeClassName="active"  to="/About" className="nav-item">About</NavLink>
+            <NavLink onClick={showMenu} activeClassName="active" exact to="/MSPORTS" className="nav-item">Home</NavLink>
+            <NavLink onClick={showMenu} activeClassName="active"  to="/BMR" className="nav-item">Bmr Calculator</NavLink>
+            <NavLink onClick={showMenu} activeClassName="active"  to="/About" className="nav-item">About</NavLink>
         </nav>
+        </div>
     )
 }
